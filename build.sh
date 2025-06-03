443b41c1314c999888602a480cf403259e76472e
Jyjays <jyjaysjiang@gmail.com>
2025-06-03 15:43:56 +0800

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # 获取libcuckoo
-git submodule add https://github.com/efficient/libcuckoo.git third_party/libcuckoo
+# git submodule add https://github.com/efficient/libcuckoo.git third_party/libcuckoo
 git submodule update --init --recursive
 
 # 获取gtest
-git submodule add https://github.com/google/googletest.git third_party/googletest
+# git submodule add https://github.com/google/googletest.git third_party/googletest
 git submodule update --init --recursive
 
-mkdir build & cd build
+mkdir build && cd build
 cmake .. 
